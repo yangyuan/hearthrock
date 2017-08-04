aa5e5a9d2b698419f2af92dda239d35439cbea66
Yang Yuan <yangyuan@users.noreply.github.com>
2017-08-04 22:52:40 +0800

    Add a new hook to make it possible to choose the solt of minions.

--- src/Hearthrock.Client/Hacking/MonoCecilExtensions.cs
+++ src/Hearthrock.Client/Hacking/MonoCecilExtensions.cs
@@ -6,6 +6,7 @@ namespace Hearthrock.Client.Hacking
 {
     using System;
 
+    using Hearthrock.Bot.Exceptions;
     using Mono.Cecil;
     using Mono.Cecil.Cil;
 
@@ -55,8 +56,7 @@ namespace Hearthrock.Client.Hacking
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString());
-                return null;
+                throw new PegasusException($"{assemblyDefinition.FullName} {typeName}.{methodName} method not found!", e);
             }
         }
 
@@ -79,8 +79,45 @@ namespace Hearthrock.Client.Hacking
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString());
-                return null;
+                throw new PegasusException("Failed to inject method.", e);
+            }
+        }
+
+        /// <summary>
+        /// Hijack a method's return value.
+        /// </summary>
+        /// <param name="assemblyDefinition">The AssemblyDefinition</param>
+        /// <param name="methodDefinition">The MethodDefinition to filter the return value.</param>
+        /// <param name="baseMethodDefinition">The MethodDefinition To Hijack</param>
+        /// <returns>The updated AssemblyDefinition.</returns>
+        public static AssemblyDefinition HijackMethod(this AssemblyDefinition assemblyDefinition, MethodDefinition methodDefinition, MethodDefinition baseMethodDefinition)
+        {
+            try
+            {
+                ILProcessor ilp = methodDefinition.Body.GetILProcessor();
+
+                // the index of temporary variable
+                int index = ilp.Body.Variables.Count;
+                ilp.Body.Variables.Add(new VariableDefinition(baseMethodDefinition.ReturnType));
+
+                var instructions = ilp.Body.Instructions.ToArray();
+
+                foreach (Instruction instruction in instructions)
+                {
+                    // for all RET operation, call the filter function to filter the value.
+                    if (instruction.OpCode == OpCodes.Ret)
+                    {
+                        ilp.InsertBefore(instruction, ilp.Create(OpCodes.Call, assemblyDefinition.MainModule.Import(baseMethodDefinition.Resolve())));
+                        ilp.InsertBefore(instruction, ilp.Create(OpCodes.Stloc, index));
+                        ilp.InsertBefore(instruction, ilp.Create(OpCodes.Ldloc, index));
+                    }
+                }
+
+                return assemblyDefinition;
+            }
+            catch (Exception e)
+            {
+                throw new PegasusException("Failed to hiject method.", e);
             }
         }
     }
---
+++ src/Hearthrock/Hooks/RockGameHooks.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="RockGameHooks.cs" company="https://github.com/yangyuan">
+//     Copyright (c) The Hearthrock Project. All rights reserved.
+// </copyright>
+
+namespace Hearthrock.Hooks
+{
+    using System;
+
+    /// <summary>
+    /// Hooks for Hearthrock
+    /// </summary>
+    public static class RockGameHooks
+    {
+        /// <summary>
+        ///  Gets or sets a value indicating whether PlayZoneSlotMousedOver is enabled.
+        /// </summary>
+        public static bool EnablePlayZoneSlotMousedOver { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of PlayZoneSlotMousedOver.
+        /// </summary>
+        public static int PlayZoneSlotMousedOverValue { get; set; }
+
+        /// <summary>
+        /// The method to filter the return value of PlayZoneSlotMousedOver.
+        /// </summary>
+        /// <param name="position">The original position</param>
+        /// <returns>The updated position</returns>
+        public static int PlayZoneSlotMousedOver(int position)
+        {
+            if (EnablePlayZoneSlotMousedOver)
+            {
+                int count = GameState.Get().GetFriendlySidePlayer().GetBattlefieldZone().GetCardCount();
+                int result = PlayZoneSlotMousedOverValue;
+
+                if (result < 0 || result > count)
+                {
+                    // By default, the position will be the middle slot
+                    result = GameState.Get().GetFriendlySidePlayer().GetBattlefieldZone().GetCardCount() / 2;
+                }
+
+                Console.WriteLine("SlotPosition " + position + " >>>>>>>>> " + result);
+                return result;
+            }
+            else
+            {
+                return position;
+            }
+        }
+    }
+}
